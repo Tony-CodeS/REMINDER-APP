0dbed26db685e390b5955baddfaf48c15e2ebe58
Tony-CodeS <officialtony484@gmail.com>
2022-06-26 13:45:09 -0700

    React counter

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Navbar.css"
+
+const Navbar =()=>{
+  return(
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Services</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar; 
